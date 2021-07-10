e9a20a0b3b16b5b0be3dcc4135b6b73f01c48e55
muna yassin <muna.yassin@gmail.com>
2021-07-11 01:34:00 +0300

    Square Sequence Test
     Check if the second sequence passed into the TestForSquares() method contains the squares of the elements in the first sequence. It should return true if the two sequences have the same number of elements and each element in squares is equal to the square of the corresponding element in numbers.

--- LINQTasks/Program.cs
+++ LINQTasks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LINQTasks
 {
@@ -7,6 +8,15 @@ namespace LINQTasks
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            IEnumerable<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            IEnumerable<int> squares = new List<int> { 1, 4, 9, 16, 25 };
+            
+            IEnumerable<int> numbers1 = new List<int> {1, 2, 5}; 
+            IEnumerable<int > squares1 = new List<int> {1, 4, 9};
+            
+            Console.WriteLine(SquareSequenceTest.TestForSquares(numbers,squares));
+            Console.WriteLine(SquareSequenceTest.TestForSquares(numbers1,squares1));
         }
     }
 }
